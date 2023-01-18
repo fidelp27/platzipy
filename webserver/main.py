f9ac03871d9035b92256aea922f9fe7bf26fa270
fidelp27 <fidel2702@gmail.com>
2023-01-17 21:47:22 -0300

    web server fastapi

---
+++ webserver/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+#Este es el endpoint
+@app.get("/")
+def get_list():
+    return [1,2,3]
+
+@app.get("/bienvenida", response_class =HTMLResponse)
+def get_letras():
+    return '''
+    <h1>Hola mundo desde fastapi</h1> 
+'''
+
+    
+if __name__ == "__main__":
+    run()    
